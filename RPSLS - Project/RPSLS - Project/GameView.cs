72d78315f98d3ad82d02503b96af097449fc6b61
Yannis Tack <yannis.tack@hotmail.com>
2015-10-21 16:38:26 +0200

    Classes en Models toegevoegd

---
+++ RPSLS - Project/RPSLS - Project/GameView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPSLS___Project
+{
+    public partial class GameView : Form
+    {
+        private HandController controller;
+        public GameView()
+        {
+            InitializeComponent();
+      
+            controller = new HandController(false);
+            
+        }
+
+    private void GameView_Load(object sender, EventArgs e)
+    {
+      HandView hView = controller.getView();
+      Controls.Add(hView);
+    }
+  }
+}
---
+++ RPSLS - Project/RPSLS - Project/HandController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS___Project
+{
+  public class HandController
+  {
+    private HandModel model;
+    private HandView view;
+
+    public HandController(bool isNpc)
+    {
+      model = new HandModel();
+      view = new HandView(this);
+      model.IsNpc = isNpc;
+    }
+
+    public HandView getView()
+    {
+      return view;
+    }
+
+    public void NpcChoice()
+    {
+      model.ChooseRandom();
+    }
+
+    public void PlayerChoice(string buttonText)
+    {
+      switch (buttonText)
+      {
+        case "Rock":
+          break;
+        case "Scissors":
+          break;
+        case "Paper":
+          break;
+        case "Lizard":
+          break;
+        case "Spock":
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
