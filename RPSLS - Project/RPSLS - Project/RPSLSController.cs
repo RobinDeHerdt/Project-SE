43091958c1e9abafa337c76cdb03739b4dbcca49
Yannis Tack <yannis.tack@hotmail.com>
2015-10-28 14:27:06 +0100

    GameLogic afgewerkt

--- RPSLS - Project/RPSLS - Project/RPSLSController.cs
+++ RPSLS - Project/RPSLS - Project/RPSLSController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RPSLS___Project
 {
-  class RPSLSController
+  public class RPSLSController
   {
     private HandController[] playerArray;
     private HandController player, npc;
@@ -16,11 +16,11 @@ namespace RPSLS___Project
     public RPSLSController()
     {
       int playerCount = 0;
-      player = new HandController(false);
+      player = new HandController(false, this);
       playerArray[playerCount] = player;
       playerCount++;
 
-      npc = new HandController(true);
+      npc = new HandController(true, this);
       playerArray[playerCount] = npc;
       playerCount++;
 
@@ -28,9 +28,123 @@ namespace RPSLS___Project
       view = new RPSLSView();
     }
 
-    private void StartGame()
+    public RPSLSView GetView()
     {
-      playerArray[model.PlayerTurn].
+      return view;
+    }
+
+    private void StartTurn()
+    {
+      playerArray[model.PlayerTurn].ChooseHand();
+    }
+
+    public void NextTurn()
+    {
+      if (model.PlayerTurn == playerArray.Length - 1)
+      {
+        CalculateWinner();
+        model.PlayerTurn = 0;
+      }
+      model.PlayerTurn++;
+      StartTurn();
+    }
+
+    private void CalculateWinner()
+    {
+      string[] handsOfChoice = new string[playerArray.Length];
+      int[] tempPoints = new int[playerArray.Length];
+      for (int i = 0; i < playerArray.Length; i++)      //  Get all handsOfChoice
+      {
+        handsOfChoice[i] = playerArray[i].GetHand();
+      }
+
+      for (int i = 0; i < handsOfChoice.Length; i++)    //  Vergelijkt alle handsOfChoice met de rest
+      {
+        for (int j = i + 1; j < handsOfChoice.Length; j++)
+        {
+          tempPoints[i] += CalculateHands(handsOfChoice[i], handsOfChoice[j]);
+        }
+      }
+
+      int winningPlayerNumber = 0;
+      for (int i = 1; i <= tempPoints.Length; i++)   // i = 1 omdat Draw = 0
+      {
+        int highestPoints = 0;
+
+        if (tempPoints[i] > highestPoints)
+        {
+          highestPoints = tempPoints[i];
+          winningPlayerNumber = i;
+        }
+      }
+    }
+
+    private int CalculateHands(string s, string t)
+    {
+      switch (s)
+      {
+        case "Rock":
+          {
+            if (t == "Rock" || t == "Paper" || t == "Spock")
+            {
+              return 0;
+            }
+            else if (t == "Scissors" || t == "Lizard")
+            {
+              return 1;
+            }
+            return 0;
+          }
+        case "Paper":
+          {
+            if (t == "Lizard" || t == "Paper" || t == "Scissors")
+            {
+              return 0;
+            }
+            else if (t == "Rock" || t == "Spock")
+            {
+              return 1;
+            }
+            return 0;
+          }
+        case "Scissors":
+          {
+            if (t == "Rock" || t == "Scissors" || t == "Spock")
+            {
+              return 0;
+            }
+            else if (t == "Paper" || t == "Lizard")
+            {
+              return 1;
+            }
+            return 0;
+          }
+        case "Lizard":
+          {
+            if (t == "Rock" || t == "Scissors" || t == "Lizard")
+            {
+              return 0;
+            }
+            else if (t == "Spock" || t == "Paper")
+            {
+              return 1;
+            }
+            return 0;
+          }
+        case "Spock":
+          {
+            if (t == "Lizard" || t == "Paper" || t == "Spock")
+            {
+              return 0;
+            }
+            else if (t == "Scissors" || t == "Rock")
+            {
+              return 1;
+            }
+            return 0;
+          }
+        default: return 0;
+      }
     }
   }
 }
